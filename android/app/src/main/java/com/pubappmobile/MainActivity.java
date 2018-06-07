622d18eb26dde81f69af47147e64a924b80de19f
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-06-08 01:30:19 +0200

    launcher and splashscreen

--- android/app/src/main/java/com/pubappmobile/MainActivity.java
+++ android/app/src/main/java/com/pubappmobile/MainActivity.java
@@ -2,6 +2,32 @@ package com.pubappmobile;
 
 import com.reactnativenavigation.controllers.SplashActivity;
 
+import android.graphics.Typeface;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.util.TypedValue;
+import android.widget.TextView;
+
 public class MainActivity extends SplashActivity {
 
+    @Override
+    public LinearLayout createSplashLayout() {
+       LinearLayout view = new LinearLayout(this);
+       TextView text = new TextView(this);
+       Typeface type = Typeface.createFromAsset(getAssets(),"fonts/RobotoCondensed-Regular.ttf");
+
+       view.setBackgroundColor(Color.parseColor("#009999"));
+       view.setGravity(Gravity.CENTER);
+
+       text.setText("Flow Academy© PubApp");
+       text.setTextColor(Color.parseColor("#ffffff"));
+       text.setGravity(Gravity.CENTER);
+       text.setTypeface(type);
+       text.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 60);
+
+       view.addView(text);
+
+       return view;
+    }
 }
